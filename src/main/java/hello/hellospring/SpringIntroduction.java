f0678df08718275defd41039025f50884aa8a482
Eunong <hoodyong@gmail.com>
2023-03-06 00:37:23 +0900

    섹션 4. 스프링 빈과 의존관계  - 컴포넌트 스캔과 자동 의존관계 설정  - 자바 코드로 직접 스프링 빈 등록하기 실습

--- src/main/java/hello/hellospring/SpringIntroduction.java
+++ src/main/java/hello/hellospring/SpringIntroduction.java
@@ -10,4 +10,9 @@ public class SpringIntroduction {
 		SpringApplication.run(SpringIntroduction.class, args);
 	}
 
+	/**
+	 * 기본적으로 @SpringBootApplication 클래스가 해당되는 패키지의 하위 클래스들만 자동 컴포넌트 스캔의 대상이 된다.
+	 * 적용되는 패키지 레벨을 변경하고 싶을 경우에는 별도의 설정을 해주어야 한다.
+	 */
+
 }
